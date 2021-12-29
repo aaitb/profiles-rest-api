38debf6ad09a4953b004ac1c964b860404c693b6
Aya A <aaitbou@ncsu.edu>
2021-12-29 18:01:35 -0500

    Added custom user profile model, manager and migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'profiles_api',
-
 ]
 
 MIDDLEWARE = [
@@ -122,6 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 
 AUTH_USER_MODEL= 'profiles_api.UserProfile'
